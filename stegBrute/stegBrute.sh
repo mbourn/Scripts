5449d8f6d01880d0ce5541de134723a33cb56d48
Matt Bourn <mbourn@mail.csuchico.edu>
2014-10-16 23:08:29 -0700

    Cleaned up the code, switched to a case statment to improve speed, added code to prevent files from being overwritten or the code skipping over a found password.

--- stegBrute/stegBrute.sh
+++ stegBrute/stegBrute.sh
@@ -1,22 +1,36 @@
 #!/bin/bash
 
+#  Get the files to be worked on from the user
 read -p "Enter the name of the steg file: " stegfile
-#read stegfile
-
 read -p "Enter the name of the word list: " wordlist
-#read wordlist
+count=0
 
-FILE="$wordlist"
-
+#  Read through the word list until the password is found or the end is reached
 while read line; do
-	#steghide extract -p $line -sf pic.bmp >> output 2>&1
-	#echo $line
-	#check=`grep -c 'wrote' output`
-	#echo $check
-	if [ `steghide extract -p $line -sf $stegfile 2>&1 | grep -c 'wrote'` -gt 0 ]; then
-		echo $line
-		exit
-	fi
-done < $FILE
+
+	#  Print out the count every 100,000 tries to let the user know the program 
+	#+  is still working
+	((count++))
+	[ `expr $count % 100000` -eq 0 ] && echo $count
+    
+	#  Capture the result of the steghide command.  Test the results to 
+	#+ determine what to do next
+    result=`steghide extract -p $line -sf $stegfile 2>&1` 
+    case $result in
+
+    	#  The password was found and the embedded file was extracted
+    	*"wrote"*) echo "The password is $line"
+				   exit;;
+		#  The password was found but there is a file in the current directory
+		#+ that has the same name as the file to be extracted.  Print 'n' to  
+		#+ prevent the overwrite, print an explaination to the user.
+		*"overwrite"*) echo n
+					   echo "------------------------------------------------"
+					   echo "There is a file in this directory with the same name as the embedded file."
+					   echo "The embedded file cannot be extracted until this conflict is resolved."
+					   echo "The password is:  $line."
+					   exit;;
+	esac
+done < $wordlist
 	
 
